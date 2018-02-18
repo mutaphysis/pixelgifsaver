1e7a12b010c1b43f27d589fa5df7974ff46f55b8
Henrik Hinrichs <henrik@mutaphysis.de>
2018-02-18 20:33:39 +0100

    Moved loading gif to background thread

--- PixelGifSaver/PixelScene.swift
+++ PixelGifSaver/PixelScene.swift
@@ -33,50 +33,52 @@ class PixelScene {
   private var currentAnimation: SKSpriteNode? = nil
   private var currentIndex = 0;
   
-  private func putAnimatedGif(url: URL) -> SKSpriteNode {
-    // load new gif
-    let gifResource = GifImage(url: url)!
-    
-    // figure out how it fits best
-    let aspectRatio = gifResource.size.width / gifResource.size.height;
-    let direction = (aspectRatio > 1.0) ? AnimationDirection.horizontal : AnimationDirection.vertical
-    
-    // determine size and movement
-    var rectangleSize: CGSize
-    var overlap: CGFloat
-    switch direction {
-      case .horizontal:
-        rectangleSize = CGSize(width: 1.0 * aspectRatio, height: 1.0)
-        overlap = rectangleSize.width - 1.0
-      case .vertical:
-        rectangleSize = CGSize(width: 1.0, height: 1.0 / aspectRatio)
-      overlap = rectangleSize.height - 1.0
+  private func putAnimatedGif(url: URL) {
+    let background = DispatchQueue.global();
+    background.async {
+      // load new gif
+      let gifResource = GifImage(url: url)!
+      
+      DispatchQueue.main.async {
+        
+        // figure out how it fits best
+        let aspectRatio = gifResource.size.width / gifResource.size.height;
+        let direction = (aspectRatio > 1.0) ? AnimationDirection.horizontal : AnimationDirection.vertical
+        
+        // determine size and movement
+        var rectangleSize: CGSize
+        var overlap: CGFloat
+        switch direction {
+        case .horizontal:
+          rectangleSize = CGSize(width: 1.0 * aspectRatio, height: 1.0)
+          overlap = rectangleSize.width - 1.0
+        case .vertical:
+          rectangleSize = CGSize(width: 1.0, height: 1.0 / aspectRatio)
+          overlap = rectangleSize.height - 1.0
+        }
+        
+        // insert to scene
+        let scene = self.scene
+        let gifNode = SKSpriteNode(color: SKColor.red, size: rectangleSize)
+        gifNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        gifNode.position = CGPoint(x: 0.5, y: 0.5)
+        scene.addChild(gifNode)
+        
+        // setup all animations on the node
+        let oscillate = SKAction.oscillation(direction: direction,
+                                             amplitude: overlap/2,
+                                             timePeriod: 10.0,
+                                             midPoint: gifNode.position)
+        gifNode.run(SKAction.repeatForever(oscillate))
+        gifNode.run(SKAction.repeatForever(gifResource.animation()))
+      }
     }
-    
-    // insert to scene
-    let scene = self.scene
-    let gifNode = SKSpriteNode(color: SKColor.red, size: rectangleSize)
-    gifNode.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-    gifNode.position = CGPoint(x: 0.5, y: 0.5)
-    scene.addChild(gifNode)
-    
-    // setup all animations on the node
-    let oscillate = SKAction.oscillation(direction: direction,
-                                         amplitude: overlap/2,
-                                         timePeriod: 10.0,
-                                         midPoint: gifNode.position)
-    gifNode.run(SKAction.repeatForever(oscillate))
-    gifNode.run(SKAction.repeatForever(gifResource.animation()))
-    
-    
-    
-    return gifNode
   }
   
   static private func prepareScene() -> SKScene {
     let scene = SKScene.init()
     scene.scaleMode = .aspectFill
-    scene.backgroundColor = SKColor.green
+    scene.backgroundColor = SKColor.black
     
     return scene
   }
@@ -85,7 +87,7 @@ class PixelScene {
     currentIndex = (currentIndex + 1) % urls.count
     
     if (urls.count > 0) {
-      self.currentAnimation = self.putAnimatedGif(url: urls[currentIndex])
+      self.putAnimatedGif(url: urls[currentIndex])
       // self.scene.run(<#T##action: SKAction##SKAction#>)
     }
   }
