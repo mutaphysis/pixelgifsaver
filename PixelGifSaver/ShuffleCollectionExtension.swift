e94688c5a6308e1f7320defe1f85b8277794dd61
Henrik Hinrichs <henrik@mutaphysis.de>
2018-02-18 19:22:05 +0100

    Added fisher yates shuffling

---
+++ PixelGifSaver/ShuffleCollectionExtension.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+extension Collection {
+    /// Return a copy of `self` with its elements shuffled
+    func shuffle() -> [Iterator.Element] {
+        var list = Array(self)
+        list.shuffleInPlace()
+        return list
+    }
+}
+
+extension MutableCollection where Index == Int {
+    /// Shuffle the elements of `self` in-place.
+    mutating func shuffleInPlace() {
+        let count = Int(self.count);
+        // empty and single-element collections don't shuffle
+        if count < 2 { return }
+        
+        for i in 0..<count - 1 {
+            let j = Int(arc4random_uniform(UInt32(count - i))) + i
+            guard i != j else { continue }
+            swapAt(i, j)
+        }
+    }
+}
